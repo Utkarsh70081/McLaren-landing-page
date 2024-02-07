d6be1049460a746351bdc73d793b24cca4090cae
Utkarsh_Mishra <97652158+Utkarsh70081@users.noreply.github.com>
2024-02-08 00:46:38 +0530

    Update js.js

--- js.js
+++ js.js
@@ -1,3 +1,31 @@
+// // TOGGLE MENU
+// function toggleMenu(){
+//     const menu = document.querySelector('.menu');
+//     const nav = document.querySelector('.nav');
+//     menu.classList.toggle('active');
+//     nav.classList.toggle('active');
+// }
+
+// // BACKGROUND VIDEO CHANGE 
+// function changeVideo(name){
+//     const bgVideoList = document.querySelectorAll('.bg-video');
+//     const models = document.querySelectorAll('.model');
+
+//     bgVideoList.forEach(video => {
+//         video.classList.remove('active');
+//         if(video.classList.contains(name)){
+//             video.classList.add('active');
+//         }
+//     });
+
+//     models.forEach(model => {
+//         model.classList.remove('active');
+//         if(model.classList.contains(name)){
+//             model.classList.add('active');
+//         }
+//     });
+// }
+
 // TOGGLE MENU
 function toggleMenu(){
     const menu = document.querySelector('.menu');
@@ -25,3 +53,4 @@ function changeVideo(name){
         }
     });
 }
+
